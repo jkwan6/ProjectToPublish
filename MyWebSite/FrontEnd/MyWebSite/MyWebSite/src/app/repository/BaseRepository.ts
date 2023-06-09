d4fc576f6e5c83ef6f624c03b698b1e5fbf697c9
Kevin <kwcw@live.com>
2023-06-09 23:45:29 +1000

    Repository updates

--- MyWebSite/FrontEnd/MyWebSite/MyWebSite/src/app/repository/BaseRepository.ts
+++ MyWebSite/FrontEnd/MyWebSite/MyWebSite/src/app/repository/BaseRepository.ts
@@ -17,7 +17,6 @@ export class BaseRepository<T> {
   constructor(private httpClient: HttpClient) { }
 
   GetAll(url: string, pageParams: IPageParams): Observable<T> {
-
     var params = new HttpParams()
     .set(this.nameof(pageParams, x => x.pageIndex), pageParams.pageIndex)
     .set(this.nameof(pageParams, x => x.pageSize), pageParams.pageSize)
@@ -30,11 +29,8 @@ export class BaseRepository<T> {
     }
 
     var queryable = this.httpClient.get<T>(url, { params });
-
     return queryable;
-
   }
-
 
   GetById(baseUrl:string, id: number): Observable<T> {
     var url = baseUrl + "/" + id;
@@ -42,12 +38,17 @@ export class BaseRepository<T> {
     return queryable;
   }
 
-  PutItem(baseUrl: string, object: T) {
+  PutItem(baseUrl: string, object: T, id: number) {
+    var url = baseUrl + "/" + id;
+    var queryable = this.httpClient.put<T>(url, object);
+    return queryable;
   }
-  //Put(item: T): Observable<T>;
 
-
-  //Post(item: T): Observable<T>;
+  Post(baseUrl: string, object: T): Observable<T> {
+    var url = baseUrl;
+    var queryable = this.httpClient.post<T>(url, object);
+    return queryable;
+  };
 
 
   //Delete(item: T): Observable<T>;
